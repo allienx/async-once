93a20275f0d6167e90799e13cd03c9c252d39f52
Alex Lin <allienx@hashmail.cc>
2019-09-07 23:34:59 -0700

    add once function and test

---
+++ src/once.test.js
@@ -0,0 +1,155 @@
+const once = require('./once')
+
+jest.useFakeTimers()
+
+describe('once', () => {
+  describe('when no argument is provided', () => {
+    it('should throw an error', () => {
+      expect(() => {
+        once()
+      }).toThrow()
+    })
+  })
+
+  describe('when the argument is not a function', () => {
+    it('should throw an error', () => {
+      const args = [123, 'asdf', [], {}]
+
+      args.forEach(arg => {
+        expect(() => {
+          once(arg)
+        }).toThrow()
+      })
+    })
+  })
+
+  describe('when the wrapped asyncFn is called multiple times and resolves', () => {
+    const asyncData = [{ test: true }]
+
+    let onceFn
+    let mockFn
+
+    beforeEach(() => {
+      mockFn = jest.fn()
+
+      onceFn = once(() => {
+        mockFn()
+
+        return new Promise(resolve => {
+          setTimeout(() => {
+            resolve(asyncData)
+          }, 1000)
+        })
+      })
+    })
+
+    it('should only call the asyncFn once', () => {
+      const promise1 = onceFn()
+      const promise2 = onceFn()
+      const promise3 = onceFn()
+
+      jest.runAllTimers()
+
+      return Promise.all([promise1, promise2, promise3]).then(() => {
+        expect(mockFn).toHaveBeenCalledTimes(1)
+      })
+    })
+
+    it('should return a unique promise to each caller', () => {
+      const promise1 = onceFn()
+      const promise2 = onceFn()
+      const promise3 = onceFn()
+
+      expect(promise1 !== promise2).toBe(true)
+      expect(promise2 !== promise3).toBe(true)
+      expect(promise1 !== promise3).toBe(true)
+    })
+
+    it('should resolve each caller with the asyncData', () => {
+      const promise1 = onceFn()
+      const promise2 = onceFn()
+      const promise3 = onceFn()
+
+      jest.runAllTimers()
+
+      return Promise.all([
+        expect(promise1).resolves.toEqual(asyncData),
+        expect(promise2).resolves.toEqual(asyncData),
+        expect(promise3).resolves.toEqual(asyncData),
+      ])
+    })
+
+    it('should resolve each caller in the order they were called', () => {
+      const testData = []
+      const promise1 = onceFn().then(() => testData.push(1))
+      const promise2 = onceFn().then(() => testData.push(2))
+      const promise3 = onceFn().then(() => testData.push(3))
+
+      jest.runAllTimers()
+
+      return Promise.all([promise1, promise2, promise3]).then(() => {
+        expect(testData).toEqual([1, 2, 3])
+      })
+    })
+  })
+
+  describe('when the wrapped asyncFn is called multiple times and rejects', () => {
+    const err = new Error('It handles reject.')
+
+    let onceFn
+    let mockFn
+
+    beforeEach(() => {
+      mockFn = jest.fn()
+
+      onceFn = once(() => {
+        mockFn()
+
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            reject(err)
+          }, 1000)
+        })
+      })
+    })
+
+    it('should only call the asyncFn once', () => {
+      const promise1 = onceFn()
+      const promise2 = onceFn()
+      const promise3 = onceFn()
+
+      jest.runAllTimers()
+
+      return Promise.all([promise1, promise2, promise3]).catch(() => {
+        expect(mockFn).toHaveBeenCalledTimes(1)
+      })
+    })
+
+    it('should reject each caller with the error', () => {
+      const promise1 = onceFn()
+      const promise2 = onceFn()
+      const promise3 = onceFn()
+
+      jest.runAllTimers()
+
+      return Promise.all([
+        expect(promise1).rejects.toEqual(err),
+        expect(promise2).rejects.toEqual(err),
+        expect(promise3).rejects.toEqual(err),
+      ])
+    })
+
+    it('should reject each caller in the order they were called', () => {
+      const testData = []
+      const promise1 = onceFn().catch(() => testData.push(1))
+      const promise2 = onceFn().catch(() => testData.push(2))
+      const promise3 = onceFn().catch(() => testData.push(3))
+
+      jest.runAllTimers()
+
+      return Promise.all([promise1, promise2, promise3]).catch(() => {
+        expect(testData).toEqual([1, 2, 3])
+      })
+    })
+  })
+})
